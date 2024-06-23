c61cdac44ea9c8c73ae798c3f9da236fe095728d
hgkim215 <hgkim215@gmail.com>
2024-06-23 16:26:37 +0900

    Initial Commit

---
+++ Memorable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Memorable
+//
+//  Created by 김현기 on 6/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
