8128b6f0dfe6c99e882e6c3656bd4d964c5543fe
hgkim215 <hgkim215@gmail.com>
2024-06-23 16:35:22 +0900

    [Init] 스토리보드 삭제

--- Memorable/ViewController.swift
+++ Memorable/ViewController.swift
@@ -8,12 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .green
         // Do any additional setup after loading the view.
     }
-
-
 }
-
