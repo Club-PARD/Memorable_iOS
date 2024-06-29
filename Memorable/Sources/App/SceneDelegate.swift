f3272e81ca30ef66d8a7cfa23381d182ead66b57
hgkim215 <hgkim215@gmail.com>
2024-06-29 16:17:28 +0900

    [Merge] 추가적인 Conflict 이슈 해결

--- Memorable/Sources/App/SceneDelegate.swift
+++ Memorable/Sources/App/SceneDelegate.swift
@@ -15,8 +15,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         window = UIWindow(windowScene: windowScene)
 
-//        let homeViewController = TestViewController()
-        let homeViewController = WorkSheetViewController()
+//        let homeViewController = WorkSheetViewController()
+        let homeViewController = HomeViewController()
         let navigationVC = UINavigationController(rootViewController: homeViewController)
 
         window?.rootViewController = navigationVC
